4f9da4385a46cc7ebb7b9af39b53c7c7c457e53a
roguecat0 <roguecat0@gmail.com>
2026-03-29 18:18:25 +0200

    calling c in rust

--- rust-ffi/src/main.rs
+++ rust-ffi/src/main.rs
@@ -3,21 +3,19 @@
 
 // use std::ffi::c_int;
 unsafe extern "C" {
-    fn strlen(s: *const std::os::raw::c_char) -> usize;
-    // fn add(a: c_int, b: c_int) -> c_int;
+    // fn strlen(s: *const std::os::raw::c_char) -> usize;
+    fn add(a: i32, b: i32) -> i32;
 }
 
 fn main() {
     // CString handles null-termination for us - C strings require a trailing \0
-    let rust_string = std::ffi::CString::new("Hello, FFI!").unwrap();
+    // let rust_string = std::ffi::CString::new("Hello, FFI!").unwrap();
 
     // We must use unsafe because the compiler cannot verify the C function's behavior
-    let length = unsafe { strlen(rust_string.as_ptr()) };
-    // let res = unsafe { add(2, 3) };
+    // let length = unsafe { strlen(rust_string.as_ptr()) };
+    let res = unsafe { add(2, 3) };
 
-    println!("String length: {}, ", length);
+    println!("String length: {}", res);
 }
-#[unsafe(no_mangle)]
-pub extern "C" fn rust_multiply(a: i32, b: i32) -> i32 {
-    a * b
-}
+#[link(name = "math_ops")]
+unsafe extern "C" {}

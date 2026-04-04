883366d7c4dac2d30df574ad7b733dd1042ac223
roguecat0 <roguecat0@gmail.com>
2026-04-04 20:55:12 +0200

    soomething

--- graphing/graph.py
+++ graphing/graph.py
@@ -5,20 +5,21 @@ import ctypes
 import os
 
 print(os.getcwd())
-lib = ctypes.CDLL("../Test_in_C/lib_lock_speed.so")
-lib.mutex_reads.argtypes = [ctypes.c_uint16, ctypes.c_uint32]
-lib.mutex_reads.restype = ctypes.c_float
+lib1 = ctypes.CDLL("../Test_in_C/lib_lock_speed.so")
+lib1.mutex_reads.argtypes = [ctypes.c_uint16, ctypes.c_uint32]
+lib1.mutex_reads.restype = ctypes.c_float
 
-lib = ctypes.CDLL("./liblock_bencher.so")
-lib.bench_result.argtypes = [ctypes.c_uint16, ctypes.c_uint32]
-lib.bench_result.restype = ctypes.c_float
+lib2 = ctypes.CDLL("../target/release/librs_locks.so")
+lib2.mutex_bench.argtypes = [ctypes.c_uint16, ctypes.c_uint32]
+lib2.mutex_bench.restype = ctypes.c_float
 
-x = list(range(1, 3))
-results1 = [lib.mutex_reads(i, 3000) for i in x]
-results2 = [lib.bench_result(i, 3000) for i in x]
+x = list(range(1, 11))
+results1 = [lib1.mutex_reads(i, 3000) for i in x]
+results2 = [lib2.mutex_bench(i, 3000) for i in x]
 results = [results1, results2]
 
-plt.plot(x, results, marker="o", linestyle="-")
+plt.plot(x, results1, marker="o", linestyle="-")
+plt.plot(x, results2, marker="x", linestyle="-")
 
 plt.xlabel("Threads")
 plt.ylabel("Reads/µS")
@@ -29,7 +30,7 @@ y_max = max([max(results1), max(results2)])
 y_start = 0
 y_end = 5 * int(y_max // 5 + 1)
 
-plt.yticks(np.arange(0, y_max + 5, 5))
+plt.yticks(np.arange(0, 105 + 5, 5))
 
 
 plt.grid(axis="y", linestyle="--", alpha=0.7)
